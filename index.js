b015c73f6ba491626cec5b1bd21cc38ad2d950a4
Marek Będkowski <marek.bedkowski@pro4people.com>
2014-01-26 22:08:32 +0100

    Removed any output during module loading - which caused node-pre-gyp hiccups.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 if (process.env.NODE_MPG123N_CI !== "true") {
     exports.Player = require('./lib/player');
 } else {
-    console.warn("mpg123n continuous integration mode - no output is supported");
-	exports.Player = function () {
-		throw new Error("mpg123n continuous integration mode - no output is supported");
-	};
+    exports.Player = function () {
+        throw new Error("mpg123n continuous integration mode - no output is supported");
+    };
 }

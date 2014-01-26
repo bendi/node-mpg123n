cdcd41ae66542f4141ecaf56e7a3b1cd268f1f62
Marek Będkowski <marek@bedkowski.pl>
2014-01-26 17:51:31 +0100

    Added ability to disable native module when running tests.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-exports.Player = require('./lib/player');
+if (process.env.NODE_MPG123N_CI !== "true") {
+    exports.Player = require('./lib/player');
+} else {
+    console.warn("mpg123n continuous integration mode - no output is supported");
+	exports.Player = function () {
+		throw new Error("mpg123n continuous integration mode - no output is supported");
+	};
+}

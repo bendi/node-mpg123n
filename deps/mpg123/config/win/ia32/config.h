788255fd40169515f381b7410184cce0c64b9819
Marek Będkowski <marek@bedkowski.pl>
2014-05-25 11:10:01 +0200

    Fixing #5 - update version info.

--- deps/mpg123/config/win/ia32/config.h
+++ deps/mpg123/config/win/ia32/config.h
@@ -47,7 +47,7 @@
 #define PACKAGE_NAME "mpg123"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mpg123 1.15.3"
+#define PACKAGE_STRING "mpg123 1.19.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mpg123"
@@ -56,7 +56,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.15.3"
+#define PACKAGE_VERSION "1.19.0"
 
 /* The suffix for module files. */
 #define MODULE_FILE_SUFFIX ".lib"

855015a477bcd4f7084dbd49cfb5bdcc04dad27e
Marek Bedkowski <marek@bedkowski.pl>
2014-05-28 00:56:16 +0300

    Fixing #5 - Updated default output to alsa

--- deps/mpg123/config/linux/x64/config.h
+++ deps/mpg123/config/linux/x64/config.h
@@ -27,7 +27,7 @@
 /* #undef DEBUG */
 
 /* The default audio output module(s) to use */
-#define DEFAULT_OUTPUT_MODULE "oss"
+#define DEFAULT_OUTPUT_MODULE "alsa,oss"
 
 /* Define if building with dynamcally linked libmpg123 */
 #define DYNAMIC_BUILD 1
